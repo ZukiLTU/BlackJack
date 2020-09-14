e86ad54bf829b70fb1e117d775eb7054a5aaceaf
ZukiLTU <49497113+ZukiLTU@users.noreply.github.com>
2020-09-14 21:34:36 +0200

    Create Cartes.cs

---
+++ Code/Cartes.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackJack
+{    class Cartes
+    {
+        public int Valeur { get; set; }
+        public string Nom { get; set; }
+        public string Image { get; set; }
+    }
+}
